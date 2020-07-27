e23b48a0c51e849d086ce8b08c6a61c2956d4637
vandit1920 <48818526+vandit1920@users.noreply.github.com>
2020-07-27 13:17:49 +0530

    Create task8.js

---
+++ TASK/TASK8/task8.js
@@ -0,0 +1,41 @@
+var userName = "Nathan";
+userName  ? console.log(`Hello ,${userName}`) : console.log("Hello");
+var userQuestion = 'What do you wanna ask?';
+console.log(userName + ', ' +userQuestion);
+// create randonm variable
+var randomNumber = Math.floor(Math.random()*8);
+console.log(randomNumber);
+var eightBall = "";
+switch(randomNumber) {
+    case 1:
+      eightBall = "It is certain";
+      break;
+    case 2:
+      eightBall = "It is decidedly so";
+      break;
+    case 3:
+     eightBall = "Reply hazy try again";
+      break;
+    case 4:
+      eightBall = "Cannot predict now";
+      break;
+    case 5:
+     eightBall = "Don't count on it";
+      break;
+    case 6:
+      eightBall = "My sources say no";
+      break;
+    case 7:
+      eightBall = "Outlook not so good";
+      break;
+    default:
+    //   eightBall = "lol";
+      break;   
+}
+console.log(eightBall);
+
+// if (randomNumber == 1) {
+//     eightBall = "It is certain";
+// } else if (randomNumber == 2) {
+//     eightBall = "It is decidedly so";
+// }
